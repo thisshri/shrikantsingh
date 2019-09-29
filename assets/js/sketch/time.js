a0d9a1d4f4a6ea30708023740b250a8064b4e74a
shri <thisshri@gmail.com>
2019-09-29 16:14:23 +0530

    Clean code

--- assets/js/sketch/time.js
+++ assets/js/sketch/time.js
@@ -1,24 +1,22 @@
 function setup() {
-    frameRate(100)
+
     let clientWidth = document.body.clientWidth
     let clientHeight = document.body.clientHeight
+    
+    frameRate(100)
     createCanvas(clientWidth, clientHeight)
+    textAlign(CENTER, CENTER);
+    
     this.PIXEL = floor(min(clientWidth, clientHeight)/10)
-
     this.timeX = floor((clientWidth / 2))
     this.timeY =  floor((clientHeight / 2))
-    textAlign(CENTER, CENTER);
-
 }
-
-this.foo = 4;
 
 function draw() {
   background(0);
   fill(255);
   textSize(this.PIXEL)
-  let date = new Date()
-  
-  let time = `${date.getHours()} : ${date.getMinutes()}  ${date.getSeconds()} ${("000" + date.getMilliseconds()).slice (-3)}`
+  const date = new Date()
+  const time = `${date.getHours()} : ${date.getMinutes()}  ${date.getSeconds()} ${("000" + date.getMilliseconds()).slice (-3)}`
   text(time, this.timeX ,this.timeY);
   }
